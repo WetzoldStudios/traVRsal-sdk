cb73c63417012eb19750b3aa07520476030384b4
Robert Wetzold <rwetzold@gmail.com>
2020-06-19 14:21:51 +0200

    Add automatic TileMap to JSON converter

---
+++ Editor/TileMapConverter.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace traVRsal.SDK
+{
+    public class TileMapConverter : AssetPostprocessor
+    {
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            List<string> changedFiles = new List<string>();
+
+            foreach (string str in importedAssets.Concat(movedAssets).Concat(movedFromAssetPaths))
+            {
+                if (str.EndsWith(TileMapUtil.MAP_EXTENSION) || str.EndsWith(TileMapUtil.WORLD_EXTENSION))
+                {
+                    changedFiles.Add(str);
+                }
+            }
+
+            if (changedFiles.Count > 0)
+            {
+                // only run if not in play-mode as otherwise we get duplicate reloads in game
+                if (!Application.isPlaying && Application.isEditor)
+                {
+                    ConvertTiledToJSON(changedFiles);
+
+                    // live reload in editor
+                    AssetDatabase.Refresh();
+                }
+            }
+        }
+
+        public static void ConvertTiledToJSON(List<string> changedFiles)
+        {
+            string tiledExe = TravrsalSettingsManager.Get<string>("tiledPath");
+
+            foreach (string file in changedFiles)
+            {
+                // leave old name intact and simply add .json
+                string targetName = file + ".json";
+
+                if (file.EndsWith(TileMapUtil.WORLD_EXTENSION))
+                {
+                    FileUtil.ReplaceFile(file, targetName);
+                }
+                else
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = tiledExe;
+                    process.StartInfo.Arguments = "--export-map JSON --embed-tilesets --resolve-types-and-properties \"" + file + "\" \"" + targetName + "\"";
+                    process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    process.Start();
+                    process.WaitForExit();
+                }
+            }
+        }
+    }
+}
