c23a0b66ab07867ae7320d4d90f94981e15c80ce
Robert Wetzold <rwetzold@gmail.com>
2020-06-19 16:39:52 +0200

    Trigger Reactor and Transform Aligner logic

---
+++ Runtime/Logic/TransformAligner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace traVRsal.SDK
+{
+    public class TransformAligner : MonoBehaviour
+    {
+        public Transform source;
+        public Vector3 positionOffset;
+
+        void Update()
+        {
+            transform.position = source.position + positionOffset;
+            transform.rotation = source.rotation;
+        }
+    }
+}
---
+++ Runtime/Logic/TriggerReactor.cs
@@ -0,0 +1,12 @@
+﻿namespace traVRsal.SDK
+{
+    public class TriggerReactor : ExecutorConfig
+    {
+        public string source = "Player";
+
+        public override string ToString()
+        {
+            return $"TriggerReactor";
+        }
+    }
+}
