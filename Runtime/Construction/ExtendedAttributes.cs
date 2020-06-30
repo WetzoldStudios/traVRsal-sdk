e3c0378c973a280c0a6fab35d9c93261a9c80b4b
Robert Wetzold <rwetzold@gmail.com>
2020-06-30 14:57:32 +0200

    New extended attribute: nonBlocking

--- Runtime/Construction/ExtendedAttributes.cs
+++ Runtime/Construction/ExtendedAttributes.cs
@@ -4,6 +4,7 @@ namespace traVRsal.SDK
 {
     public class ExtendedAttributes : MonoBehaviour
     {
+        public bool nonBlocking = false;
         public bool climbable = false;
         public bool portalHole = false;
         public BasicEntity.Direction initialDirection = BasicEntity.Direction.SOUTH;
