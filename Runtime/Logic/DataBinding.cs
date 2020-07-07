06bbc7050f3bc2d1804f7648ed60ad58f5b4d9fa
Robert Wetzold <rwetzold@gmail.com>
2020-07-07 13:45:55 +0200

    Introduction of Data Binding

---
+++ Runtime/Logic/DataBinding.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace traVRsal.SDK
+{
+    public class DataBinding : ExecutorConfig
+    {
+        public enum Reference
+        {
+            OriginalValue = 0,
+            AppVersion = 1,
+            DebugMode = 7,
+            DoOpenLevelDetailsDialog = 18,
+            DoCloseLevelDetailsDialog = 13,
+            DoOpenSettingsDialog = 20,
+            DoCloseSettingsDialog = 21,
+            DoPause = 15,
+            DoReturnToMain = 17,
+            DoStartLevel = 14,
+            DoUnpause = 16,
+            LevelCategory = 10,
+            LevelCover = 19,
+            LevelLongDescription = 12,
+            LevelName = 2,
+            LevelShortDescription = 11,
+            LevelTime = 4,
+            Padding = 6,
+            PlayerDistance = 9,
+            PlayerName = 3,
+            TileCount = 5,
+            TileSizeHint = 8
+        }
+
+        public Behaviour targetComponent;
+        public Reference reference;
+        public bool oneTimeOnly = false;
+    }
+}
