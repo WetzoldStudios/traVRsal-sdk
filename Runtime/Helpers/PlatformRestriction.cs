e30c7315a762b2f1ebdbe8e6bd9cdd919316735d
Robert Wetzold <rwetzold@gmail.com>
2021-01-10 18:41:39 +0100

    Helper script: Platform Restriction

---
+++ Runtime/Helpers/PlatformRestriction.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace traVRsal.SDK
+{
+    public class PlatformRestriction : MonoBehaviour
+    {
+        public RuntimePlatform[] platforms;
+
+        private void Start()
+        {
+            bool valid = platforms.Any(p => p == Application.platform);
+            if (!valid) gameObject.SetActive(false);
+        }
+    }
+}
