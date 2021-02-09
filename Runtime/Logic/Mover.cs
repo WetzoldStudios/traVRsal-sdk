ada89ddafbb0a14a43a351c98e3f8cd38a7c1713
Robert Wetzold <rwetzold@gmail.com>
2021-02-09 16:57:57 +0100

    New logic component: Mover for constant movement

---
+++ Runtime/Logic/Mover.cs
@@ -0,0 +1,20 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace traVRsal.SDK
+{
+    public class Mover : MonoBehaviour
+    {
+        public Vector2 distance = new Vector2(1f, 1f);
+        public float duration = 4f;
+        public Vector3 axis = Vector3.up;
+        public Ease easeType = Ease.InOutSine;
+        public LoopType loopType = LoopType.Yoyo;
+
+        private void Start()
+        {
+            float finalDistance = Random.Range(distance.x, distance.y);
+            transform.DOLocalMove(transform.localPosition + axis * finalDistance, duration).SetLoops(-1, loopType).SetEase(easeType);
+        }
+    }
+}
