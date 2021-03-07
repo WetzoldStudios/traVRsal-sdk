201067b46f4d09a0cca452ada77e60b222b40180
Robert Wetzold <rwetzold@gmail.com>
2021-03-08 00:17:54 +0100

    First version of spawner component

---
+++ Runtime/Logic/Spawner.cs
@@ -0,0 +1,16 @@
+﻿namespace traVRsal.SDK
+{
+    public class Spawner : ExecutorConfig
+    {
+        public enum Mode
+        {
+            Periodic = 0,
+            MaxAmount = 1
+        }
+
+        public Mode mode = Mode.Periodic;
+        public string[] objects;
+        public float delay = 2f;
+        public bool randomRotation = true;
+    }
+}
