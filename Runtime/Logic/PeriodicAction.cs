ba6fd7aea4b020ede895d9c0a98ec6bec6da030d
Robert Wetzold <rwetzold@gmail.com>
2021-03-21 18:00:23 +0100

    New logic component: periodic action

---
+++ Runtime/Logic/PeriodicAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace traVRsal.SDK
+{
+    public class PeriodicAction : MonoBehaviour
+    {
+        [Header("Timing")] public Vector2 initialDelay = new Vector2(1f, 2f);
+        public Vector2 interval = new Vector2(3f, 4f);
+        public bool fixedInterval = true;
+
+        [Header("Action")] public List<GameObject> toggleObjects;
+        public List<Behaviour> toggleComponents;
+        public List<Collider> toggleColliders;
+
+        private float finalInterval;
+        private float nextAction;
+
+        private void Start()
+        {
+            finalInterval = Random.Range(interval.x, interval.y);
+            nextAction = Time.time + Random.Range(initialDelay.x, initialDelay.y) + (fixedInterval ? finalInterval : Random.Range(interval.x, interval.y));
+        }
+
+        private void Update()
+        {
+            if (Time.time > nextAction)
+            {
+                nextAction = Time.time + (fixedInterval ? finalInterval : Random.Range(interval.x, interval.y));
+
+                toggleObjects.ForEach(go => go.SetActive(!go.activeSelf));
+                toggleComponents.ForEach(b => b.enabled = !b.enabled);
+                toggleColliders.ForEach(b => b.enabled = !b.enabled);
+            }
+        }
+    }
+}
