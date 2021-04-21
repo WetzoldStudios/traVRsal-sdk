a41eae7cb6464d8facd5bf04193582b6e546941e
Robert Wetzold <rwetzold@gmail.com>
2021-04-21 16:58:29 +0200

    Damageable can affect game objects on destruction

--- Runtime/Logic/Damageable.cs
+++ Runtime/Logic/Damageable.cs
@@ -11,12 +11,16 @@ namespace traVRsal.SDK
         public GameObject destructionEffect;
         public Vector3 destructionEffectOffset;
 
+        public List<GameObject> enabledGameObjectssOnDestruction;
+        public List<GameObject> disabledGameObjectsOnDestruction;
         public List<Behaviour> enabledComponentsOnDestruction;
         public List<Behaviour> disabledComponentsOnDestruction;
         public AudioSource[] breakSounds;
 
         public Damageable()
         {
+            enabledGameObjectssOnDestruction = new List<GameObject>();
+            disabledGameObjectsOnDestruction = new List<GameObject>();
             enabledComponentsOnDestruction = new List<Behaviour>();
             disabledComponentsOnDestruction = new List<Behaviour>();
         }
