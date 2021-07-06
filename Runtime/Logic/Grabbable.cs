f5139264816cdf6b4b50d64e2bba23e370ad8a0c
Robert Wetzold <rwetzold@gmail.com>
2021-07-06 23:45:59 +0200

    Grabbable helper

---
+++ Runtime/Logic/Grabbable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace traVRsal.SDK
+{
+    [AddComponentMenu("traVRsal/Grabbable")]
+    public class Grabbable : ExecutorConfig
+    {
+        [Header("On Grab")]
+        public bool hideHand = true;
+
+        [Header("On Drop")]
+        public bool activateGravity = true;
+        public bool deactivateKinematic = true;
+    }
+}
