165274526895f195ec12966f778efdc426d0de46
Robert Wetzold <rwetzold@gmail.com>
2021-07-14 10:24:28 +0200

    New logic component: Hide On Proximity

---
+++ Runtime/Logic/HideOnProximity.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace traVRsal.SDK
+{
+    [AddComponentMenu("traVRsal/Hide on Proximity")]
+    public class HideOnProximity : MonoBehaviour
+    {
+        [Header("Static References")] public Transform trackedObject;
+
+        [Tooltip("Object which the distance is measured against. Defaults to current object.")]
+        public Transform referenceObject;
+
+        [Tooltip("Object which visibility should be affected.")]
+        public GameObject managedObject;
+
+        [Header("Configuration")] public float distance = 0.5f;
+
+        private void Start()
+        {
+            if (referenceObject == null) referenceObject = transform;
+        }
+
+        private void Update()
+        {
+            float measuredDistance = Mathf.Abs((trackedObject.position - referenceObject.position).magnitude);
+            bool visible = measuredDistance >= distance;
+            if (managedObject.activeSelf != visible) managedObject.SetActive(visible);
+        }
+    }
+}
