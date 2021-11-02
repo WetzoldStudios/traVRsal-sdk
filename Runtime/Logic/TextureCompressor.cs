cd62f3493380cbaac3325c7da2f8cf553d330a35
Robert Wetzold <rwetzold@gmail.com>
2021-11-02 20:40:02 +0100

    New helper component: Texture Compressor, to reduce texture size at runtime

---
+++ Runtime/Logic/TextureCompressor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace traVRsal.SDK
+{
+    [AddComponentMenu("traVRsal/Texture Compressor")]
+    public class TextureCompressor : MonoBehaviour
+    {
+        public bool includeInactive = true;
+
+        private void Awake()
+        {
+            GetComponentsInChildren<Renderer>(includeInactive).ForEach(r => r.materials.ForEach(m =>
+            {
+                Shader shader = m.shader;
+                for (int i = 0; i < shader.GetPropertyCount(); i++)
+                {
+                    if (shader.GetPropertyType(i) != ShaderPropertyType.Texture) continue;
+
+                    Texture2D tex = (Texture2D) m.GetTexture(shader.GetPropertyName(i));
+                    if (tex != null) tex.Compress(true);
+                }
+            }));
+        }
+    }
+}
